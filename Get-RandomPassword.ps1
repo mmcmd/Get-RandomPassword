08eebc2eb87a4a4b074300ee09614a31d066e0ac
mmcmd <36875145+mmcmd@users.noreply.github.com>
2019-12-15 12:57:52 -0500

    Added a function block to the main function

--- Get-RandomPassword.ps1
+++ Get-RandomPassword.ps1
@@ -1,166 +1,168 @@
-<#
-.SYNOPSIS
-    Generates a random password using random common words
-.DESCRIPTION
-    This cmdlet generates a series of random words which are easy to remember and have fairly high entropy. Based on the xkcd comic (https://xkcd.com/936/)
-    Short words: 1 to 4 characters long
-    Medium words: 5-8 characters long (default)
-    Long words: 9+ characters long
-.EXAMPLE
-    Get-RandomPassword -Words 3
-.EXAMPLE
-    Get-RandomPassword -Words 5 -Delimiter "-"
-.PARAMETER Words
-    Number of words that the password will contain. The default is 3. Example: "car-HORSE-staple" is a 3 word password
-.PARAMETER Delimiter
-    Separator for the passwords. Default is a dash (-). Example: "car!HORSE!staple" the delimiter here would be the exclamation point (!)
-.Parameter Count
-    Number of passwords you want generated. Default is 3.
-.PARAMETER short
-    Specifies to take from the list of short words (1-4 characters long)
-.PARAMETER medium
-    Specifies to take from the list of medium words (5-8 characters long) (default length if no length is specified)
-.PARAMETER long
-    Specifies to take from the list of long words (9+ characters long)
-.PARAMETER NoCapitalization
-    Specifies that all words will be in lowercase
-.PARAMETER DoNotCopyToClipboard
-    Will not copy a random password to your clipboard
-.PARAMETER French
-    Generates a password using french words
-.NOTES
-    Strong, simple password generation
-.FUNCTIONALITY
-    Strong, simple password generation
-#>
+function Get-RandomPassword{
+    <#
+    .SYNOPSIS
+        Generates a random password using random common words
+    .DESCRIPTION
+        This cmdlet generates a series of random words which are easy to remember and have fairly high entropy. Based on the xkcd comic (https://xkcd.com/936/)
+        Short words: 1 to 4 characters long
+        Medium words: 5-8 characters long (default)
+        Long words: 9+ characters long
+    .EXAMPLE
+        Get-RandomPassword -Words 3
+    .EXAMPLE
+        Get-RandomPassword -Words 5 -Delimiter "-"
+    .PARAMETER Words
+        Number of words that the password will contain. The default is 3. Example: "car-HORSE-staple" is a 3 word password
+    .PARAMETER Delimiter
+        Separator for the passwords. Default is a dash (-). Example: "car!HORSE!staple" the delimiter here would be the exclamation point (!)
+    .Parameter Count
+        Number of passwords you want generated. Default is 3.
+    .PARAMETER short
+        Specifies to take from the list of short words (1-4 characters long)
+    .PARAMETER medium
+        Specifies to take from the list of medium words (5-8 characters long) (default length if no length is specified)
+    .PARAMETER long
+        Specifies to take from the list of long words (9+ characters long)
+    .PARAMETER NoCapitalization
+        Specifies that all words will be in lowercase
+    .PARAMETER DoNotCopyToClipboard
+        Will not copy a random password to your clipboard
+    .PARAMETER French
+        Generates a password using french words
+    .NOTES
+        Strong, simple password generation
+    .FUNCTIONALITY
+        Strong, simple password generation
+    #>
 
-[CmdletBinding(DefaultParameterSetName="medium")]
-param (
-    [Parameter()] # Number of words that the password will contain. Default is 3
-    [int]
-    $Words = 3,
-    [Parameter()] # Delimiter for the words. Default is a dash (-)
-    [string]
-    $Delimiter = "-",
-    [Parameter()] # Number of passwords that the script will generate. Default is 3
-    [int]
-    $Count = 3,
-    [Parameter(ParameterSetName="short")] # Selects short words (1-4 chars)
-    [switch]
-    $short,
-    [Parameter(ParameterSetName="medium")] # Selects medium words (5-8 chars)
-    [switch]
-    $medium,
-    [Parameter(ParameterSetName="long")] # Selects long words (9+ chars)
-    [switch]
-    $long,
-    [Parameter()]
-    [switch]
-    $NoCapitalization,
-    [Parameter()]
-    [switch]
-    $DoNotCopyToClipboard,
-    [Parameter()]
-    [switch]
-    $French
-)
+    [CmdletBinding(DefaultParameterSetName="medium")]
+    param (
+        [Parameter()] # Number of words that the password will contain. Default is 3
+        [int]
+        $Words = 3,
+        [Parameter()] # Delimiter for the words. Default is a dash (-)
+        [string]
+        $Delimiter = "-",
+        [Parameter()] # Number of passwords that the script will generate. Default is 3
+        [int]
+        $Count = 3,
+        [Parameter(ParameterSetName="short")] # Selects short words (1-4 chars)
+        [switch]
+        $short,
+        [Parameter(ParameterSetName="medium")] # Selects medium words (5-8 chars)
+        [switch]
+        $medium,
+        [Parameter(ParameterSetName="long")] # Selects long words (9+ chars)
+        [switch]
+        $long,
+        [Parameter()]
+        [switch]
+        $NoCapitalization,
+        [Parameter()]
+        [switch]
+        $DoNotCopyToClipboard,
+        [Parameter()]
+        [switch]
+        $French
+    )
 
-$ErrorActionPreference = "Stop"
+    $ErrorActionPreference = "Stop"
 
 
-Get-ChildItem -path $PSScriptRoot\functions\*.ps1 | ForEach-Object { # Import the functions in the functions folder
-    Write-Verbose "Loaded $($_.fullname)"
-    . $_.FullName
-}
+    Get-ChildItem -path $PSScriptRoot\functions\*.ps1 | ForEach-Object { # Import the functions in the functions folder
+        Write-Verbose "Loaded $($_.fullname)"
+        . $_.FullName
+    }
 
-try{
-    Write-Verbose "Importing dictionnaries"
-    switch ($PSCmdlet.ParameterSetName) {
-        short {
-            if ($French -ne $true){ 
-                Write-Verbose -Message "Short english words selected. Getting content from $PSScriptRoot\ressources\google-10000-english-usa-no-swears-short.txt"
-                $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\google-10000-english-usa-no-swears-short.txt"
+    try{
+        Write-Verbose "Importing dictionnaries"
+        switch ($PSCmdlet.ParameterSetName) {
+            short {
+                if ($French -ne $true){ 
+                    Write-Verbose -Message "Short english words selected. Getting content from $PSScriptRoot\ressources\google-10000-english-usa-no-swears-short.txt"
+                    $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\google-10000-english-usa-no-swears-short.txt"
+                }
+                else{
+                    Write-Verbose -Message "Short french words selected. Getting content from $PSScriptRoot\ressources\french-short-words.txt"
+                    $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\french-short-words.txt" -Encoding UTF8
+                }
             }
-            else{
-                Write-Verbose -Message "Short french words selected. Getting content from $PSScriptRoot\ressources\french-short-words.txt"
-                $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\french-short-words.txt" -Encoding UTF8
+            medium {
+                if ($French -ne $true){
+                    Write-Verbose -Message "Medium english words selected. Getting content from $PSScriptRoot\ressources\google-10000-english-usa-no-swears-medium.txt"
+                    $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\google-10000-english-usa-no-swears-medium.txt"
+                }
+                else{
+                    Write-Verbose -Message "Medium french words selected. Getting content from $PSScriptRoot\ressources\french-medium-words.txt"
+                    $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\french-medium-words.txt" -Encoding UTF8
+                }
             }
-        }
-        medium {
-            if ($French -ne $true){
-                Write-Verbose -Message "Medium english words selected. Getting content from $PSScriptRoot\ressources\google-10000-english-usa-no-swears-medium.txt"
-                $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\google-10000-english-usa-no-swears-medium.txt"
-            }
-            else{
-                Write-Verbose -Message "Medium french words selected. Getting content from $PSScriptRoot\ressources\french-medium-words.txt"
-                $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\french-medium-words.txt" -Encoding UTF8
-            }
-        }
-        long {
-            if ($French -ne $true){
-                Write-Verbose -Message "Long english words selected. Getting content from $PSScriptRoot\ressources\google-10000-english-usa-no-swears-long.txt"
-                $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\google-10000-english-usa-no-swears-long.txt"
-            }
-            else{
-                Write-Verbose -Message "Long french words selected. Getting content from $PSScriptRoot\ressources\french-long-words.txt"
-                $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\french-long-words.txt" -Encoding UTF8
+            long {
+                if ($French -ne $true){
+                    Write-Verbose -Message "Long english words selected. Getting content from $PSScriptRoot\ressources\google-10000-english-usa-no-swears-long.txt"
+                    $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\google-10000-english-usa-no-swears-long.txt"
+                }
+                else{
+                    Write-Verbose -Message "Long french words selected. Getting content from $PSScriptRoot\ressources\french-long-words.txt"
+                    $dictionnary = Get-Content -Path "$PSScriptRoot\ressources\french-long-words.txt" -Encoding UTF8
+                }
             }
         }
     }
-}
-catch [ItemNotFoundException]{
-    Write-RedText -message  "The required dictionnary files are not present. Please make sure the required files are in the ressources folder."
-    exit
-}
-catch{
-    Write-ErrorMessage -Message "An error occured importing the dictionaries! See below for more information:"
-    exit
-}
-
-
-$Collection = New-Object System.Collections.Generic.List[string]
-
-1..$Count | ForEach-Object{
-    try{
-        Write-Verbose "Getting random words from the dictionnary. Currently at iteration number $_"
-        $random_words = (Get-Random -Count $Words -InputObject $dictionnary)
+    catch [ItemNotFoundException]{
+        Write-RedText -message  "The required dictionnary files are not present. Please make sure the required files are in the ressources folder."
+        exit
     }
     catch{
-        Write-ErrorMessage -Message "An error occured getting random words from the dictionnary"
+        Write-ErrorMessage -Message "An error occured importing the dictionaries! See below for more information:"
         exit
     }
 
-    if ($NoCapitalization -eq $false){
+
+    $Collection = New-Object System.Collections.Generic.List[string]
+
+    1..$Count | ForEach-Object{
         try{
-            Write-Verbose "Randomly capitalizing each word. Currently at iteration number $_"
-            $random_words = [string]($random_words | ForEach-Object { $Caps = Get-Random -InputObject ($true,$false); if ($Caps -eq $true) { $_.ToUpper() } else{ $_.ToLower() } }) # Randomly capitalizes a word
+            Write-Verbose "Getting random words from the dictionnary. Currently at iteration number $_"
+            $random_words = (Get-Random -Count $Words -InputObject $dictionnary)
         }
         catch{
-            Write-ErrorMessage -Message "An error occured with the random capitalization of the passwords"
+            Write-ErrorMessage -Message "An error occured getting random words from the dictionnary"
+            exit
+        }
+
+        if ($NoCapitalization -eq $false){
+            try{
+                Write-Verbose "Randomly capitalizing each word. Currently at iteration number $_"
+                $random_words = [string]($random_words | ForEach-Object { $Caps = Get-Random -InputObject ($true,$false); if ($Caps -eq $true) { $_.ToUpper() } else{ $_.ToLower() } }) # Randomly capitalizes a word
+            }
+            catch{
+                Write-ErrorMessage -Message "An error occured with the random capitalization of the passwords"
+            }
+        }
+        else{
+            Write-Verbose "Capitalization has been toggled off"
+            $random_words = [string]$random_words
+        }
+
+        try{
+            Write-Verbose "Adding the delimiter to each word, Currently at iteration number $_"
+            . Add-Delimiter -Source $random_words -DelimiterParameter $Delimiter
+            $Collection.Add($sanitized)
+        }
+        catch{
+            Write-ErrorMessage -Message "An error occured adding a delimiter to the passwords"
         }
     }
-    else{
-        Write-Verbose "Capitalization has been toggled off"
-        $random_words = [string]$random_words
-    }
 
-    try{
-        Write-Verbose "Adding the delimiter to each word, Currently at iteration number $_"
-        . Add-Delimiter -Source $random_words -DelimiterParameter $Delimiter
-        $Collection.Add($sanitized)
-    }
-    catch{
-        Write-ErrorMessage -Message "An error occured adding a delimiter to the passwords"
-    }
-}
-
-if ($DoNotCopyToClipboard -eq $false){
-    try{
-        Write-Verbose "Getting a random passwword from the list"
-        Get-Random -InputObject $Collection -Count 1 | Set-Clipboard
-        Write-Host "A random password has been copied to your clipboard!" -ForegroundColor Green -BackgroundColor Black
-    }
-    catch{
-        Write-ErrorMessage -Message "An error occured getting a random password"
+    if ($DoNotCopyToClipboard -eq $false){
+        try{
+            Write-Verbose "Getting a random passwword from the list"
+            Get-Random -InputObject $Collection -Count 1 | Set-Clipboard
+            Write-Host "A random password has been copied to your clipboard!" -ForegroundColor Green -BackgroundColor Black
+        }
+        catch{
+            Write-ErrorMessage -Message "An error occured getting a random password"
+        }
     }
 }
